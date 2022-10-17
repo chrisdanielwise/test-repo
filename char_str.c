ffb8ff826bc529fde603d9b43cc2403bbe44cd1e
Henry <kanuchigozie7@gmail.com>
2022-10-17 19:50:40 +0000

    create a funtion for char and string

---
+++ char_str.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_char - prints characters;
+ * @arg: List of argument
+ * Return: returns an unsigned char to an int.
+ */
+int print_char(va_list arg)
+{
+	return (_putchar(va_arg(arg, int)));
+}
+
+/**
+ * print_string - prints string;
+ * @arg: List of argument
+ * Return: returns length of string on success
+ */
+int print_string(va_list arg)
+{
+	int len;
+	char *str;
+
+	str = (va_arg(arg, char *));
+
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * print_percent - prints percentage symbol
+ * @arg: List of argument
+ * Return: returns an int
+ */
+int print_percent(va_list arg)
+{
+	putchar('%');
+	return (1);
+}
